886bb1f4c41b79250041b514516266fff337b50f
Arón Díaz <arondiaz.cpn@gmail.com>
2024-10-16 20:02:19 -0300

    created connection with mysql database and configure enviroment variables

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,29 @@
 import express from "express";
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 app.listen(8800, () => {
   console.log("Connected to backend!");
