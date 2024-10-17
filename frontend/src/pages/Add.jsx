ec8ce5257d11e5ceb85b704d4af98c55df46b020
Arón Díaz <arondiaz.cpn@gmail.com>
2024-10-17 16:01:26 -0300

    implemented option to add new book

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,9 +1,84 @@
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
-}
+  const [input, setInput] = useState({
+    title: "",
+    description: "",
+    price: "",
+    cover: "",
+  });
 
-export default Add
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (
+      input.title.trim() === "" ||
+      input.description.trim() === "" ||
+      input.price.trim() === ""
+    ) {
+      return;
+    }
+
+    const url = "http://localhost:8800/books";
+    try {
+      const { title, description, price, cover } = input;
+      await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          price,
+          cover,
+        }),
+      });
+      navigate("/");
+      console.log("Ok");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Add new book!</h1>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="title"
+          onChange={handleChange}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="description"
+          onChange={handleChange}
+          name="description"
+        />
+        <input
+          type="number"
+          placeholder="price"
+          onChange={handleChange}
+          name="price"
+        />
+        <input
+          type="text"
+          placeholder="cover"
+          onChange={handleChange}
+          name="cover"
+        />
+      </div>
+      <button onClick={handleClick}>ADD</button>
+    </div>
+  );
+};
+
+export default Add;
