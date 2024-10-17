a899a9af90da5b676b0f96f564bd1921c2d69e92
Arón Díaz <arondiaz.cpn@gmail.com>
2024-10-17 14:56:40 -0300

    displayed data of database on main page

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,9 +1,41 @@
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Books = () => {
-  return (
-    <div>Books</div>
-  )
-}
+  const [books, setBooks] = useState([]);
 
-export default Books
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await fetch("http://localhost:8800/books");
+        const data = await res.json();
+        setBooks(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>The book shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div key={book.id} className="book">
+            {book.cover && <img src={book.cover} alt="" />}
+
+            <h3>{book.title}</h3>
+            <p>{book.description}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+      <Link to={"/add"}>
+        <button>Add book</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Books;
