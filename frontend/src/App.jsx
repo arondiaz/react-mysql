8546edf7e5e3a6e879ba580f316a47bc129ab5f7
Arón Díaz <arondiaz.cpn@gmail.com>
2024-10-17 14:00:39 -0300

    configured react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-const App = () => {
-  return <div>
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
+import Update from "./pages/Update";
+import Add from "./pages/Add";
 
-  </div>;
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
